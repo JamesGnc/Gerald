1304a68fd6d94fe0bddd6d427cf776f8fc6681ea
LaiGc <1796487364@qq.com>
2024-10-16 13:49:26 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/Gerald/'  // 替换为你的仓库名称
+    : '/',
   plugins: [
     vue({
       template: {
